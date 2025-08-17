fec3f6c0f614dfb36f01a72675f0a22719a1b6bc
IgnacioIbanezSala <103463656+IgnacioIbanezSala@users.noreply.github.com>
2025-08-17 15:55:43 -0300

    Added Tests

--- Tests/TimeSynCn_test.py
+++ Tests/TimeSynCn_test.py
@@ -17,6 +17,11 @@ C_no_sc1a = []
 C_no_eve = []
 Eb_no_sc1a = []
 Eb_no_eve = []
+Xmtr_gain_sc1a = []
+Xmtr_gain_eve = []
+range_sc1a = []
+range_eve = []
+elevation_eve = []
 for i in range(span):
     
     idx_sc1a = df_sc1a.index[df_sc1a["Time"] == df_eve.at[i, "Time"]]
@@ -27,6 +32,11 @@ for i in range(span):
         C_no_eve.append(df_eve.loc[i]["C/No"])
         Eb_no_sc1a.append(df_sc1a.loc[idx_sc1a]["Eb/No"])
         Eb_no_eve.append(df_eve.loc[i]["Eb/No"])
+        Xmtr_gain_sc1a.append(df_sc1a.loc[idx_sc1a]["Xmtr Gain"])
+        Xmtr_gain_eve.append(df_eve.loc[i]["Xmtr Gain"])
+        range_sc1a.append(df_sc1a.loc[idx_sc1a]["Range"])
+        range_eve.append(df_eve.loc[i]["Range"])
+        elevation_eve.append(df_eve.loc[i]["Xmtr Elevation"])
         error.append((df_sc1a.loc[idx_sc1a]["C/No"]-df_eve.loc[i]["C/No"]))
                 
 error_graph = plt.subplots(1,1)[1]
@@ -39,21 +49,48 @@ plt.show()
 
 print('Mean error: ', np.mean(error))
 
-range_graph = plt.subplots(1,2)[1]
+range_graph = plt.subplots(3,2)[1]
 
-range_graph[0].plot(C_no_sc1a, color="blue", label='C/No SC1A', ls='--')
-range_graph[0].plot(C_no_eve, color="orange", label='C/No Eve', marker='x', ls=(0, (5, 10)))
-range_graph[0].set_title("C/No comprarision between SC1A and Eve")
-range_graph[0].set_ylabel('C/No [dB]')
-range_graph[0].set_xlabel('Simulation Step')
-range_graph[0].legend()
+range_graph[0][0].plot(C_no_sc1a, color="blue", label='C/No SC1A', ls='--')
+range_graph[0][0].plot(C_no_eve, color="orange", label='C/No Eve', marker='x', ls=(0, (5, 10)))
+range_graph[0][0].set_title("C/No comprarision between SC1A and Eve")
+range_graph[0][0].set_ylabel('C/No [dB]')
+range_graph[0][0].set_xlabel('Simulation Step')
+range_graph[0][0].legend()
 
-range_graph[1].plot(Eb_no_sc1a, color="blue", label='Eb/No SC1A', ls='--')
-range_graph[1].plot(Eb_no_eve, color="orange", label='Eb/No Eve', marker='x', ls=(0, (5, 10)))
-range_graph[1].set_title("Eb/No comprarision between SC1A and Eve")
-range_graph[1].set_ylabel('Eb/No [dB]')
-range_graph[1].set_xlabel('Simulation Step')
-range_graph[1].legend()
+range_graph[0][1].plot(Eb_no_sc1a, color="blue", label='Eb/No SC1A', ls='--')
+range_graph[0][1].plot(Eb_no_eve, color="orange", label='Eb/No Eve', marker='x', ls=(0, (5, 10)))
+range_graph[0][1].set_title("Eb/No comprarision between SC1A and Eve")
+range_graph[0][1].set_ylabel('Eb/No [dB]')
+range_graph[0][1].set_xlabel('Simulation Step')
+range_graph[0][1].legend()
+
+range_graph[1][0].plot(Xmtr_gain_sc1a, color="blue", label='Xmtr Gain SC1A', ls='--')
+range_graph[1][0].plot(Xmtr_gain_eve, color="orange", label='Xmtr Gain Eve', marker='x', ls=(0, (5, 10)))
+range_graph[1][0].set_title("Xmtr Gain comprarision between SC1A and Eve")
+range_graph[1][0].set_ylabel('Xmtr Gain [dB]')
+range_graph[1][0].set_xlabel('Simulation Step')
+range_graph[1][0].legend()
+
+range_graph[1][1].plot(Xmtr_gain_sc1a, color="blue", label='Xmtr Gain SC1A', ls='--')
+range_graph[1][1].plot(Xmtr_gain_eve, color="orange", label='Xmtr Gain Eve', marker='x', ls=(0, (5, 10)))
+range_graph[1][1].set_title("Xmtr Gain comprarision between SC1A and Eve")
+range_graph[1][1].set_ylabel('Xmtr Gain [dB]')
+range_graph[1][1].set_xlabel('Simulation Step')
+range_graph[1][1].legend()
+
+range_graph[2][0].plot(range_sc1a, color="blue", label='Range SC1A', ls='--')
+range_graph[2][0].plot(range_eve, color="orange", label='Range Eve', ls=(0, (5, 10)))
+range_graph[2][0].set_title("Range comprarision between SC1A and Eve")
+range_graph[2][0].set_ylabel('Range [Km]')
+range_graph[2][0].set_xlabel('Simulation Step')
+range_graph[2][0].legend()
+
+range_graph[2][1].plot(elevation_eve, color="orange", label='Range Eve', ls=(0, (5, 10)))
+range_graph[2][1].set_title("Eve elevation")
+range_graph[2][1].set_ylabel('Elevation')
+range_graph[2][1].set_xlabel('Simulation Step')
+range_graph[2][1].legend()
 
 plt.tight_layout()
 plt.show()
